58930c25bbeae4560cfd40e3d02c5472515dcc55
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-05-24 21:54:14 +0300

    Moore and Neumann neighborhoods fixed

--- game-elfi.cpp/neighbors.cpp
+++ game-elfi.cpp/neighbors.cpp
@@ -17,6 +17,8 @@ unsigned int live(wpoint[][wheight]) {
 
 void neighbors(signed int nb[][2], unsigned int x, unsigned int y) {
 	// nb[][] is NeighBors
+	// x - how much neighbors with main tile
+	// y - how many tiles in radius of main tile 
 
   unsigned int i, j;
   unsigned int k = 0;
@@ -34,7 +36,7 @@ void neighbors(signed int nb[][2], unsigned int x, unsigned int y) {
 }
 
 
-unsigned int neighbors_N(wpoint[][wheight], unsigned int x, unsigned int y) { // Neumann neighborhood
+unsigned int neighbors_M(wpoint[][wheight], unsigned int x, unsigned int y) { // Moore neighborhood
   unsigned int count = 0;
   unsigned int i;
   signed int nb[8][2];
@@ -54,7 +56,34 @@ unsigned int neighbors_N(wpoint[][wheight], unsigned int x, unsigned int y) { //
       continue;
     }
 
-    if (world[_x][_y].is_live == 1) {
+    if (world[_x][_y].live == 1) {
+		  ++count;
+    }
+  }
+  return count;
+}
+
+unsigned int neighbors_N(wpoint[][wheight], unsigned int x, unsigned int y) { // Neumann neighborhood
+  unsigned int count = 0;
+  unsigned int i;
+  signed int nb[4][2];
+	signed int _x, _y;
+
+  neighbors(nb, x, y);
+
+  for (i = 0; i < 4; ++i) {
+    _x = nb[i][0];
+    _y = nb[i][1];
+
+    if (_x < 0 || _y < 0) {
+      continue;
+    }
+
+    if (_x >= wwidth || _y >= wheight) {
+      continue;
+    }
+
+    if (world[_x][_y].live == 1) {
 		  ++count;
     }
   }
