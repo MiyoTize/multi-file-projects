e1702f7503d858035db871cb749f2794d0122b10
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-24 21:02:06 +0300

    Update world.cpp
    tabs fixed

--- game-elfi.cpp/world.cpp
+++ game-elfi.cpp/world.cpp
@@ -3,21 +3,20 @@
 int wpoint, wwidth, wheight;
 
 void world(wpoint([][wheight]) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(1, 10000);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(1, 10000);
 
-	unsigned i, j;
+  unsigned i, j;
 
-	for (i = 0, i < wwidth, i++) {
-		for (j = 0, j < wheight, j++) {
-			unsigned int num = dis(gen);
-			if (num % 2 == 0) {
-				world[i][j].live = 1;
-			} else {
-				world[i][j].live = 0;
-			}
-		}
-	}
-	return 0;
+  for (i = 0, i < wwidth, i++) {
+    for (j = 0, j < wheight, j++) {
+      unsigned int num = dis(gen);
+      if (num % 2 == 0) {
+        world[i][j].live = 1;
+      } else {
+        world[i][j].live = 0;
+      }
+    }
+  }
 }
