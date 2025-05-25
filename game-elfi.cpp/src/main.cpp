4f0c90618381b95014c905e84c369426b179b4c8
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-25 23:15:15 +0300

    Create main.cpp
    Miyo really dead @_@

---
+++ game-elfi.cpp/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+#include <fiend.h>
+#include <neighbors.h>
+#include <generations.h>
+
+main() {
+  std::cout << ">_< Miyo is dead" << std::endl;
+}
