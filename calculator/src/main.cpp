33a2b3ec7f098670f82886fc0c015a5832d299cf
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-27 00:14:19 +0300

    Update main.cpp

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -12,8 +12,10 @@ int main() {
     switch(answer) {
       case 1:
         BasedMath(double x, double y);
+        return 0;
       case 2:
         ArithmeticProgression(double a, double b, double s, double result, int n);
+        return 0;
     }
     std::cout << "Do you want solve something again?\n(Y/n): ";
     std::cin >> z;
