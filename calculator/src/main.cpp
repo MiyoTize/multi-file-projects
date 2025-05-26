fa3ff76293f9585eec72ffa957fd2b45d96bb07b
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-05-27 00:03:56 +0300

    added progression

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Text.h"
 #include "BasedMath.h"
+#include "ArithmeticProgression"
 
 int main() {
   int z;
@@ -8,10 +9,13 @@ int main() {
   std::cout << "Start?\n(Y/n): ";
   std::cin >> z;
   while (z == 'y') {
-    if (answer = 1) {
-			BasedMath(double x, double y);
-    }
-    std::cout << "Do you want solve something again?\n(Y/n): ";
+    switch(answer) {
+			case 1:
+			  BasedMath(double x, double y);
+			case 2:
+			  ArithmeticProgression(double a, double b, double s, double n);
+		}
+		std::cout << "Do you want solve something again?\n(Y/n): ";
     std::cin >> z;
 	}
   return 0;
