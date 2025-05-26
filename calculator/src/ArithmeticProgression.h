685114b329ddc4b3f15661939cd1fc0000f70f8d
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-27 00:11:47 +0300

    Update ArithmeticProgression.h

--- calculator/src/ArithmeticProgression.h
+++ calculator/src/ArithmeticProgression.h
@@ -1,18 +1,18 @@
 #include <iostream>
 
-int ArithmeticProgression(double a, double b, double s, int n) {
+int ArithmeticProgression(double a, double b, double s, double result, int n) {
   int choice;
-	std::cout << "Your choice:"
-            << "Arithmetic Progression"
-						<< "What you want find?"
-						<< "1. S"
-						<< "2. A"
-						<< "3. B"
-						<< "4. N"
-						<< "Please, write number here: ";
+  std::cout << "Your choice:"
+/*@,0*/     << "Arithmetic Progression"
+/*Sourface*/<< "What you want find?"
+/*Crazey*/  << "1. S"
+/*Tizey */  << "2. A"
+/*The lazy*/<< "3. B"
+/*Kombucha*/<< "4. N"
+/*Here yo!*/<< "Please, write number here: ";
   std::cin >> choice;
   switch(choice) {
-	  case 1:
+    case 1:
       std::cout << "\nWrite a, b and n: ";
       std::cin >> a >> b >> n;
       if(n!=1 && b!=0) {
