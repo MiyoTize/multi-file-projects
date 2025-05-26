9cbf5bfb913d80ab2806a87a744a19850d1c6c74
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-27 00:13:34 +0300

    Update main.cpp

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -10,13 +10,13 @@ int main() {
   std::cin >> z;
   while (z == 'y') {
     switch(answer) {
-			case 1:
-			  BasedMath(double x, double y);
-			case 2:
-			  ArithmeticProgression(double a, double b, double s, double n);
-		}
-		std::cout << "Do you want solve something again?\n(Y/n): ";
+      case 1:
+        BasedMath(double x, double y);
+      case 2:
+        ArithmeticProgression(double a, double b, double s, double result, int n);
+    }
+    std::cout << "Do you want solve something again?\n(Y/n): ";
     std::cin >> z;
-	}
+  }
   return 0;
 }
