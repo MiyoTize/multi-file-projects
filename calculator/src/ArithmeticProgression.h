0e64e927972b0b0cd59d237f8c3443755561cc33
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-05-26 23:57:53 +0300

    yes

---
+++ calculator/src/ArithmeticProgression.h
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int ArithmeticProgression(double a, double b, double s, int n) {
+  int choice;
+	std::cout << "Your choice:"
+            << "Arithmetic Progression"
+						<< "What you want find?"
+						<< "1. S"
+						<< "2. A"
+						<< "3. B"
+						<< "4. N"
+						<< "Please, write number here: ";
+  std::cin >> choice;
+  switch(choice) {
+	  case 1:
+      std::cout << "\nWrite a, b and n: ";
+      std::cin >> a >> b >> n;
+      if(n!=1 && b!=0) {
+        result = a + b*(n-1); // sum
+      } else {
+        std::cout << "Fucking failed!";
+        return 0;
+      }
+
+    case 2:
+      std::cout << "\nWrite s, b and n: ";
+      std::cin >> s >> b >> n;
+      if(n!=1 && b!=0) {  
+        result = s - b*(n-1); // first penis
+      } else {
+        std::cout << "Fucking failed!";
+        return 0;
+      }
+
+    case 3:
+      std::cout << "\nWrite s, a and n: ";
+      std::cin >> s >> a >> n;
+      if(n!=0 && a!=0 && s!=0) {  
+        result = s / (a * n); // diff
+      } else {
+        std::cout << "Fucking failed!";
+        return 0;
+      }
+
+    case 4:
+      std::cout << "\nWrite s, a and b: ";
+      std::cin >> s >> a >> b;
+      int z = s - a; // the костыль for if
+      if(z!=0 && b!=0) {  
+        result = (s - a) / b; // quantity
+      } else {
+        std::cout << "Fucking failed!";
+        return 0;
+      }
+  }
+}
