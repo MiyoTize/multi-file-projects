eec0a19a7d82e6f1e800ffc1d0ed03105bd80142
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-26 18:22:26 +0300

    Update Fun.h

--- calculator/src/Fun.h
+++ calculator/src/Fun.h
@@ -3,15 +3,15 @@
 
 int Fun(int x, int y, int z) { // z is out
   int w = Text(int answer);
-	switch(answer) {
+  switch(answer) {
     case 1:
-			std::cout << "Please, write x and y:\n";
+      std::cout << "Please, write x and y:\n";
       std::cin >> x >> y;
 
       std::cout << "x + y = " << x + y << "\n"
 /* fun */       << "x - y = " << x - y << "\n"
 /* much fun */  << "y - x = " << y - x << "\n"
-	              << "x * y = " << x * y << "\n"
+                << "x * y = " << x * y << "\n"
 /* ewhahaha! */ << "pow(x, y) = " << pow(x, y) << "\n"
                 << "pow(y, x) = " << pow(y, x) << "\n"
 /*Crazey Tizey*/<< "round(x) = " << round(x) << "\n"
@@ -23,7 +23,7 @@ int Fun(int x, int y, int z) { // z is out
       } else {
         std::cout << "(sqrt): Fucking fail!" << "\n";
         return 0;
-			}
+      }
 
       if(x>=0 && y>=0) {
         std::cout << "log(x) = " << log(x) << "\n"
@@ -31,7 +31,7 @@ int Fun(int x, int y, int z) { // z is out
       } else {
         std::cout << "(log): Fucking fail!" << "\n";
         return 0;
-			}
+      }
 
       if(x!=0 && y!=0) {
         std::cout << "x / y = " << x / y << "\n" 
@@ -40,6 +40,6 @@ int Fun(int x, int y, int z) { // z is out
       else {
         std::cout << "(/): Fucking fail!" << "\n";
         return 0;
-			}
+      }
   }
 }
