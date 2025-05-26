9570b8001b8bdbb0224fd8f4935cda26373c93e3
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-26 18:20:09 +0300

    Update Text.h

--- calculator/src/Text.h
+++ calculator/src/Text.h
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int Text() {
-	int answer;
+  int answer;
   std::cout << "Heyo! Here Miyo!\n"
             << "Please, write what you want:\n"
-						<< "1. Based Math ( + - * / ^ sqrt pow log round % ! )\n"
-						<< "Write here: ";
-	std::cin >> answer;
-	if (answer < 1 && answer > 1) { // bcs now here only one variant
+            << "1. Based Math ( + - * / ^ sqrt pow log round % ! )\n"
+            << "Write here: ";
+  std::cin >> answer;
+  if (answer < 1 && answer > 1) { // bcs now here only one variant
     std::cout << "Fucking fail!";
-		return 0;
+      return 0;
   }
 }
   
