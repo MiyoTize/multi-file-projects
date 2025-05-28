e45e1317cecade8399dd32c0b36139060e131dcc
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-28 23:30:30 +0300

    Update Text.h

--- calculator/src/Text.h
+++ calculator/src/Text.h
@@ -5,9 +5,10 @@ int Text(int answer) {
             << "Please, write what you want:\n"
             << "1. Based Math ( + - * / ^ sqrt pow log round % ! )\n"
             << "2. ArithmeticProgression\n"
+            << "3. Equations\n"
             << "Write here: ";
   std::cin >> answer;
-  if (answer < 1 && answer > 2) { // bcs now here only one variant
+  if (answer < 1 && answer > 3) { // bcs now here only one variant
     std::cout << "Fucking fail!";
     return 0;
   }
