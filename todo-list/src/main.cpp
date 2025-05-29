f9391d3669ae0d948674d0c4d248c085df6b7361
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-29 22:23:11 +0300

    Update main.cpp

--- todo-list/src/main.cpp
+++ todo-list/src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include "Text.h"
 #include "TodoListArray.h"
 
 int main() {
-  int Text(answer);
+  std::cout << "Heyo! Miyo here!\n";
+            << "Do you want start my Todo-list?\n(Y/n): ";
+  char answer;
+  std::cin >> answer;
   while (answer == 'y') {
-  int TodoListArray(array[x]);
+  int TodoListArray(array[]);
     std::cout << "Do you want start again?\n(Y/n): ";
     std::cin >> answer;
   }
