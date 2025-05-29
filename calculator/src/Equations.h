8a8a58c6d1921c2cbaf42590e07120cd3e295330
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-29 23:58:14 +0300

    Update Equations.h

--- calculator/src/Equations.h
+++ calculator/src/Equations.h
@@ -1,6 +1,6 @@
 #ifndef EQUATIONS_H
 #define	EQUATIONS_H
 
-void first(double a, double b, double x);
+void Equations(double x1, double x2, double a, double b, double c, double D);
 
 #endif // ! EQUATIONS_H
