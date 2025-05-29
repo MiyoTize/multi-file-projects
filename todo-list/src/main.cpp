4e4fcd6cab5f3cdf1cc849b8c76d805040a7e727
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-05-29 21:34:31 +0300

    doesnt work

--- todo-list/src/main.cpp
+++ todo-list/src/main.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include "Text.h"
+#include "TodoListArray.h"
+
+int main() {
+	int Text(answer)
+  while (answer == 'y') {
+		int TodoListArray(array[x])
+    std::cout << "Do you want start again?\n(Y/n): ";
+		std::cin >> answer;
+	}
+}
