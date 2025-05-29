9d1ed4bbf29707eb20262b53f3598a9db00d5b0b
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-29 21:49:07 +0300

    Update TodoListArray.cpp
    DUCKING TABSSS

--- todo-list/src/TodoListArray.cpp
+++ todo-list/src/TodoListArray.cpp
@@ -9,26 +9,26 @@ int TodoListArray(char* array[i]) {
   if(i == 0) {
     std::cout << "Oops! You no have tasks!\n";
   } else {
-		x = 0;
-		while( x <= i ) {
-			std::cout << x << ". " << array[i-x];
+    x = 0;
+    while( x <= i ) {
+      std::cout << x << ". " << array[i-x];
       ++x;
     }
-	}
-	while(answerarr == 'y') {
-	  std::cout << "What do you want?\n"
+  }
+  while(answerarr == 'y') {
+    std::cout << "What do you want?\n"
               << "1. add task\n"
-				    	<< "2. delete task yaaay\n";
+	      << "2. delete task yaaay\n";
     std::cin >> choice;
     if(choice == 1) { 
       std::cout << "Write task\nHere: ";
-			std::cin >> array[++i];
+      std::cin >> array[++i];
       ++i;
-		} else {
-			std::cout << "Write number of task\nHere: ";
-			std::cin >> x;
-			array[x] = 0;
+    } else {
+      std::cout << "Write number of task\nHere: ";
+      std::cin >> x;
+      array[x] = 0;
     }
-	}
+  }
 }
     
